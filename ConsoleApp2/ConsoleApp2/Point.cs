cfd586c49d9be66e832b4c543231e6f6417ebb11
huuga <huuga@inbox.ru>
2021-04-04 21:42:19 +0300

    Lesson-3

---
+++ ConsoleApp2/ConsoleApp2/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sym);
+        }
+    }
+}
