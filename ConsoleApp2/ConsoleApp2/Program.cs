778fb5591452884c4374179cf99ea26d7486556c
huuga <huuga@inbox.ru>
2021-04-05 12:36:23 +0300

    Добавил функцию VerticalLine

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp2
 {
@@ -7,13 +8,30 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Point p2 = new Point(4, 5, '#');
 
-            Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
-            p2.Draw();
+            List<int> numList = new List<int>();
+            numList.Add(21);
+            numList.Add(11);
+            numList.Add(90);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            List<Point> pointsList = new List<Point>();
+            pointsList.Add(p1);
+            pointsList.Add(p2);
+
+            int r = pointsList[1].x;
+
+            HorizontalLine Line1 = new HorizontalLine();
+            Line1.Draw();
+
+            VerticalLine line2 = new VerticalLine(8, 24, 9, '$');
+            line2.Draw();
+
+            Console.ReadLine();
         }
 
 
